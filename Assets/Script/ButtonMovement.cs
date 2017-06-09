66d73292791859b7ee4e644a2429d4095b061e17
marlene <e0037078@u.nus.edu>
2017-06-09 13:21:36 +0800

    initial commit

---
+++ Assets/Script/ButtonMovement.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovement : TouchManager {
+    // drag script to each button, then select accordingly
+    public enum type {LeftButton, RightButton, JumpButton};
+    public type buttonType;
+
+    public float jumpHeight = 0.0f; //
+    public float moveSpeed = 0.0f; //
+
+    public GameObject playerObject = null;
+    Rigidbody2D playerRigidbody;
+    Animator playerAnim;
+    bool facingRight;
+    bool isJumping;
+    
+    public GUITexture buttonTexture = null;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        playerRigidbody = playerObject.GetComponent<Rigidbody2D>();
+        playerAnim = playerObject.GetComponent<Animator>();
+        facingRight = true;
+        isJumping = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        touchInput(buttonTexture);
+	}
+
+    void OnFirstTouchBegan() // only for one frame
+    {
+        switch (buttonType)
+        {
+            case type.JumpButton:
+                playerRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                playerAnim.SetInteger("State", 2);
+                isJumping = true;
+                break;
+        }
+    }
+
+    void OnSecondTouchBegan() // only for one frame
+    {
+        switch (buttonType)
+        {
+            case type.JumpButton:
+                playerRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                playerAnim.SetInteger("State", 2);
+                isJumping = true;
+                break;
+        }
+    }
+
+    void OnFirstTouch() //  for subsequent frames
+    { 
+        switch (buttonType)
+        {
+            case type.LeftButton:
+                if (facingRight)
+                {
+                    Flip();
+                }
+                if (!isJumping)
+                {
+                    playerAnim.SetInteger("State", 1);
+                }
+                playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+            case type.RightButton:
+                if (!facingRight)
+                {
+                    Flip();
+                }
+                if (!isJumping)
+                {
+                    playerAnim.SetInteger("State", 1);
+                }
+                playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+        }
+    }
+
+    void OnSecondTouch() // for subsequent frames
+    {
+        switch (buttonType)
+        {
+            case type.LeftButton:
+                if (facingRight)
+                {
+                    Flip();
+                }
+                if (!isJumping)
+                {
+                    playerAnim.SetInteger("State", 1);
+                }
+                playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+            case type.RightButton:
+                if (!facingRight)
+                {
+                    Flip();
+                }
+                if (!isJumping)
+                {
+                    playerAnim.SetInteger("State", 1);
+                }
+                playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                break;
+        }
+    }
+
+    void OnFirstTouchEnded()
+    {
+        switch (buttonType)
+        {
+            case type.JumpButton:
+                isJumping = false;
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+                break;
+        }
+
+    }
+
+    void OnSecondTouchEnded()
+    {
+        switch (buttonType)
+        {
+            case type.JumpButton:
+                isJumping = false;
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+                break;
+        }
+    }
+
+    void Flip()
+    {
+            facingRight = !facingRight;
+            Vector3 temp = playerObject.transform.localScale;
+            temp.x *= -1;
+            playerObject.transform.localScale = temp;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "GROUND")
+        {
+            isJumping = false;
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+            playerAnim.SetInteger("State", 0);
+        }
+    }
+}
---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float speedX;
+    public float jumpSpeedY;
+
+    bool facingRight = true;
+    bool isJumping = false;
+    float speed;
+
+    Animator anim;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // move right, stop
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            speed = -speedX;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            speed = 0;
+        }
+
+        // move left, stop
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            speed = speedX;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            speed = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isJumping = true;
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeedY));
+            anim.SetInteger("State", 2);
+        }
+
+        MovePlayer(speed); // player movement
+
+        Flip();
+    }
+
+    // code for player movement and animation
+    void MovePlayer(float playerSpeed)
+    {
+        if (!isJumping && (playerSpeed < 0 || playerSpeed > 0))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (!isJumping && playerSpeed == 0)
+        {
+            anim.SetInteger("State", 0);
+        }
+        rb.velocity = new Vector3(playerSpeed, rb.velocity.y, 0);
+
+    }
+    
+    void Flip()
+    {
+        if (speed>0 && !facingRight || speed<0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 temp = transform.localScale;
+            temp.x *= -1;
+            transform.localScale = temp;
+        }
+    }
+
+    // for jump
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "GROUND")
+        {
+            isJumping = false;
+            speed = 0;
+            anim.SetInteger("State", 1);
+        }
+    }
+}
