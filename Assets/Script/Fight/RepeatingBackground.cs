f229044561c3cc22c5f8b49f179f72472fc79353
marlene <e0037078@u.nus.edu>
2017-06-25 11:58:40 +0800

    replaced fight scene background and floor

--- Assets/Script/Fight/RepeatingBackground.cs
+++ Assets/Script/Fight/RepeatingBackground.cs
@@ -23,7 +23,7 @@ public class RepeatingBackground : MonoBehaviour {
     private void Update()
     {
         //Check if the difference along the x axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
-        if (transform.position.x + groundHorizontalLength +10f < FightManager.currPlayer.transform.position.x)
+        if (transform.position.x + groundHorizontalLength +20f < FightManager.currPlayer.transform.position.x)
         {
             //If true, this means this object is no longer visible and we can safely move it forward to be re-used.
             RepositionBackground();
@@ -35,7 +35,7 @@ public class RepeatingBackground : MonoBehaviour {
     {
         
         //This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
-        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 4f + 2f, 0);// sizeOfBackground = 24f but collider 11f
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 4f + 24f, 0);// sizeOfBackground = 24f but collider 11f
 
         //Move this object from it's position offscreen, behind the player, to the new position off-camera in front of the player.
         transform.position = (Vector2)transform.position + groundOffSet;
